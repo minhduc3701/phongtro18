862d449b30993e0cd2ac9eb28b3b288abf50ffde
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2022-08-22 22:23:49 +0700

    SuccessMaster

--- src/asyncComponent.js
+++ src/asyncComponent.js
@@ -39,6 +39,7 @@ export default function asyncComponent(importComponent) {
       return (
         <ReactPlaceholder type="text" rows={7} ready={Component !== null}>
           {Component}
+          <h1>1111</h1>
         </ReactPlaceholder>
       );
     }
