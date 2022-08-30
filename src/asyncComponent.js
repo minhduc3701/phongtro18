de14fa2afba917b9a8bed8fa07ed909570a9066d
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2022-08-30 15:15:05 +0700

    remove line

--- src/asyncComponent.js
+++ src/asyncComponent.js
@@ -39,7 +39,6 @@ export default function asyncComponent(importComponent) {
       return (
         <ReactPlaceholder type="text" rows={7} ready={Component !== null}>
           {Component}
-          <span>ddmkz</span>
         </ReactPlaceholder>
       );
     }
