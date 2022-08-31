6466f80f8c299cceb4218b6a8cda07e79da66d93
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2022-08-31 13:54:18 +0700

    fix menu title 1master

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -54,7 +54,7 @@ class MenuHeader extends React.Component {
             style={{ fontSize: "1.2em" }}
             className="text-grey menu-hover text-upper h-100 p-h-3 font-weight-bold gx-link"
           >
-            Trang chủ
+            Trang chủuuuuu
           </Link>
         </Menu.Item>
         <Menu.Item key="1">
